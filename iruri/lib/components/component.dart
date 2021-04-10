3dc0f3cf054104ebc5f826e06d770b995e59917c
jswboseok <jswboseok@naver.com>
2021-04-10 18:43:15 +0900

    프로필정보 layout

--- iruri/lib/components/component.dart
+++ iruri/lib/components/component.dart
@@ -3,6 +3,106 @@ import 'package:flutter/material.dart';
 // light gray 색 구분선
 const Widget divider = Divider(color: Color(0xFFEEEEEE), thickness: 1);
 
+class MyProfile extends StatefulWidget {
+  @override
+  _MyProfileState createState() => _MyProfileState();
+}
+
+class _MyProfileState extends State<MyProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10 , horizontal: 10),
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 0), //left, top, right, bottom
+      child: Column(children: <Widget>[
+        divider,
+        Row(               //프로필 위젯 상단 왼쪽 텍스트 부분
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("프로필 정보", style: TextStyle()),
+              /*
+              * 상단 오른쪽엔 수정하기 아이콘
+              * 이미지 나중에 바꾸기
+              */
+            Image.asset('Icon-192.png', width: 30, height: 30),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+          Container(              //프로필사진 컨테이너
+            height: 130, 
+            width: 130,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Image.asset('Icon-192.png')
+          ),
+          Column(               //프로필 내용 컨테이너(닉네임, 포지션, 연락처, 이메일)
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              nickName(),
+              position(),
+              phoneNum(),
+              email(),
+          ]),
+        ]),
+        divider
+      ],)
+      
+    );
+  }
+
+  //닉네임 나타냄(임시)
+  Widget nickName(){
+    return Container(
+      child: Row(
+        children: [
+        Text("닉네임", style: TextStyle(),),
+        Text("석운장", style: TextStyle(),)
+      ],)
+    );
+  }
+
+  // 포지션 나타냄(임시)
+  Widget position(){
+    return Container(
+      child: Row(children: [
+        Text("포지션", style: TextStyle(),),
+        /*
+        * 포지션을 그리드뷰로
+        *
+        */
+        Text("그리드뷰 들어갈 자리")
+      ])
+    );
+  }
+
+  //전화번호 나타냄(임시)
+  Widget phoneNum(){
+    return Container(
+      child: Row(
+        children: [
+        Text("연락처", style: TextStyle(),),
+        Text("010-1111-1111", style: TextStyle(),)
+      ],)
+    );
+  }
+
+  //이메일 나타냄(임시)
+  Widget email(){
+    return Container(
+      child: Row(
+        children: [
+        Text("이메일", style: TextStyle(),),
+        Text("rrrrrrrr@gmail.com", style: TextStyle(),)
+      ],)
+    );
+  }
+}
+
 /**
  *  TODO : @jswboseok 프로필정보만들기
  *  1. 프로필 정보는 StatefulWidget으로 만들 것
--- iruri/lib/pages/personal/personal.dart
+++ iruri/lib/pages/personal/personal.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iruri/components/component.dart';
 
 class PersonalPage extends StatefulWidget {
   @override
   _PersonalPageState createState() => _PersonalPageState();
+  MyProfile myProfile() => MyProfile();
 }
 
 class _PersonalPageState extends State<PersonalPage> {
@@ -11,60 +13,9 @@ class _PersonalPageState extends State<PersonalPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children :<Widget>[
-        Text("PERSONAL"), 
-        myProfileContainer(),
-      ])
-    );
-  }
-
-   Widget myProfileContainer() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10 , horizontal: 10),
-      padding: EdgeInsets.fromLTRB(10, 0, 10, 0), //left, top, right, bottom
-      child: Column(children: <Widget>[
-        Row(               //프로필 위젯 상단 바 왼쪽 텍스트 부분
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "프로필 정보",
-              style: TextStyle(
-                /*
-                * 추후 디자인
-                *
-                */
-              )
-              ),
-              /*
-              * 상단 바 오른쪽엔 편집모양 아이콘
-              * 편집모양 아이콘 넣어야 해요
-              */
-            Image.asset('Icon-192.png', width: 30, height: 30),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[   // 그 아래 contents 부분
-          Container(              //프로필사진 컨테이너
-            height: 130, 
-            width: 130,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.orange,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Image.asset('Icon-192.png'),
-          ),
-          Column(               //프로필 내용 컨테이너
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("닉네임"),
-              Text("포지션"),
-              Text("연락처"),
-              Text("이메일")
-          ]),
-          Column()
-        ],)
+      child: Column(children: [
+        Text("Personal"),
+        MyProfile(),
       ],)
     );
   }
