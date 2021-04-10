eb3fa23aebb68a8c536507b072eaa56bb0b4458d
Seunghwanly <seunghwanly@gmail.com>
2021-04-10 16:02:46 +0900

    Color Updated

--- iruri/lib/components/palette.dart
+++ iruri/lib/components/palette.dart
@@ -3,4 +3,12 @@ import 'package:flutter/material.dart';
 const Color themeLighyOrange = Color.fromRGBO(242, 162, 12, 1);
 const Color themeDeepBlue = Color.fromRGBO(27, 48, 89, 1);
 const Color themeLightGrayOpacity20 = Color.fromRGBO(196, 196, 196, 0.13);
-const Color themeGrayText = Color.fromRGBO(119,119,119,0.9);
+const Color themeGrayText = Color.fromRGBO(119, 119, 119, 0.9);
+
+// tags
+const Color tagDessin = Color.fromRGBO(235, 87, 87, 0.2);
+const Color tagDraw = Color.fromRGBO(242, 135, 5, 0.2);
+const Color tagCharacter = Color.fromRGBO(187, 107, 217, 0.2);
+const Color tagConti = Color.fromRGBO(47, 128, 237, 0.2);
+const Color tagPaint = Color.fromRGBO(111, 207, 151, 0.2);
+const Color tagWrite = Color.fromRGBO(189, 189, 189, 0.2);
